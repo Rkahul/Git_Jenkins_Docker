b2242d31a2f6a55d51d47f03245a525c2a9f99ad
Rahul Dhus <rahuldhus766@gmail.com>
2020-06-08 11:57:06 +0530

    modified line in python code

--- python.py
+++ python.py
@@ -4,4 +4,4 @@ print("Thank for this tutorial")
 
 print("Hi my name is Ritvik and i am 9 months old`")
 
-print("My mom is very fat and my father is really good and talented person")
+print("My mom is very good and talented girl")
